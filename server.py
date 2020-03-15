91d7a22566cb28b42aa66a1269ebfd841d38d317
You Song Hou <yousonghou@yahoo.com>
2020-03-14 20:39:06 -0400

    set up sqlite

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
 
 app = Flask(__name__)
 
@@ -8,6 +11,12 @@ if ENV == "dev":
     app.debug = True
 else:
     app.debug = False
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 if __name__ == "__main__":
     app.run()
