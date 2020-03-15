a35157a0a7c2f428ca388ef03996eb77b33adf64
You Song Hou <yousonghou@yahoo.com>
2020-03-15 01:12:26 -0400

    add a class for user

---
+++ petexchanger/models.py
@@ -0,0 +1,14 @@
+from petexchanger import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    password = db.Column(db.String(50), nullable = False)
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
